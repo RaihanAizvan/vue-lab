e24c390d5ed53cece0fb18d4d33e49045331d0ba
Mohammed Raihan <aizvanr@gmail.com>
2025-11-25 11:49:58 +0530

    add: computed properties

---
+++ src/components/demos/Computed_Property.vue
@@ -0,0 +1,17 @@
+<!-- * computer property is a value that depends on other state -->
+<!-- and it automatically updates when that state chanegs  -->
+
+
+<script setup>
+
+import { computed, ref } from "vue";
+const firstName = ref('raihan');
+const lastName = ref("muhammed");
+const fullName = computed(() => firstName.value + ' ' + lastName.value);
+
+</script>
+
+<template>
+    <h1> Full Name : {{ fullName }}</h1>
+</template>
+
